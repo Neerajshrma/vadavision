997b7119650ca59f77b07ed4c57f912bb68394b5
unknown <prashant@vadavision.ca>
2023-09-12 12:11:40 +0530

    UI  FIX

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Homepage from "./components/Homepage/Homepage";
 import UIUXPage from "./components/UIUXPage/UIUXPage";
 import LandingPage from "./components/landingpage/LandingPage";
+import MobileApp from "./components/landingpage/OurServices/MobileApp";
 
 export default function Home() {
   return (
