adb8fba9fa43a38f3521d6f1dd8b92714e4011eb
unknown <prashant@vadavision.ca>
2023-09-13 16:09:58 +0530

    about us page

--- src/app/components/AboutUsPage/AboutUsPage.jsx
+++ src/app/components/AboutUsPage/AboutUsPage.jsx
@@ -3,6 +3,17 @@ import Header from "../UIUXPage/Header/Header";
 import Herosection from "./Herosection/Herosection";
 import AboutUs from "./AboutUs/AboutUs";
 import MissionVision from "./Missionvision/MissionVision";
+import OurCustomers from "../UIUXPage/OurSatisfyCustomers/OurCustomers";
+import WhyChooseUs from "../Homepage/Whychooseus/WhyChooseUs";
+import ConsultUs from "../landingpage/Consultus/ConsultUs";
+import ConsultWithUs from "../Homepage/Consultwithus/ConsultWithUs";
+import FAQs from "../landingpage/FAQs/FAQs";
+import Footer from "../common/Footer/Footer";
+import SubscribeUs from "../Homepage/Subscribeus/SubscribeUs";
+import FAQTabs from "../Homepage/FAQs/FAQTabs";
+import ServiseCards from "./ServiceCards/ServiseCards";
+import OurWorkCulture from "./OurWorkCulture/OurWorkCulture";
+import OurExperts from "./OurExperts/OurExperts";
 
 const AboutUsPage = () => {
   return (
@@ -17,7 +28,35 @@ const AboutUsPage = () => {
         <AboutUs />
       </div>
       <div>
-        <MissionVision/>
+        <MissionVision />
+      </div>
+      <div>
+        <ServiseCards/>
+      </div>
+      <div>
+        <OurWorkCulture/>
+      </div>
+      <div>
+        <OurExperts/>
+      </div>
+      <div>
+        <OurCustomers />
+      </div>
+      <div>
+        <WhyChooseUs />
+      </div>
+      <div>
+        <ConsultWithUs />
+      </div>
+      <div className="flex flex-row">
+        <FAQs />
+        <FAQTabs />
+      </div>
+      <div>
+        <SubscribeUs />
+      </div>
+      <div>
+        <Footer />
       </div>
     </div>
   );
