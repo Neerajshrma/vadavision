fca85a8f3f77ccb5b7bc29aa1e9e888b8b98e6cb
Neeraj sharma <shrmanerj.sn@gmail.com>
2023-09-22 14:22:33 +0530

    create pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,6 @@
-import AboutUsPage from "./components/AboutUsPage/AboutUsPage";
 import ContactUsPage from "./components/ContactUsPage/ContactUsPage";
 import Homepage from "./components/Homepage/Homepage";
-import UIUXPage from "./components/UIUXPage/UIUXPage";
-import LandingPage from "./components/landingpage/LandingPage";
-import MobileApp from "./components/landingpage/OurServices/MobileApp";
+
 
 export default function Home() {
   return (
