137baeffa39b6d7bc5f5767de5f2e16d925999d1
unknown <prashant@vadavision.ca>
2023-09-22 19:24:38 +0530

    Vadavision

---
+++ pages/aboutuspage.js
@@ -0,0 +1,12 @@
+import AboutUsPage from '@/app/components/AboutUsPage/AboutUsPage'
+import React from 'react'
+
+const aboutuspage = () => {
+  return (
+    <div>
+        <AboutUsPage/>
+    </div>
+  )
+}
+
+export default aboutuspage
---
+++ pages/uiuxpage.js
@@ -0,0 +1,12 @@
+import UIUXPage from '@/app/components/UIUXPage/UIUXPage'
+import React from 'react'
+
+const uiuxpage = () => {
+  return (
+    <div>
+        <UIUXPage/>
+    </div>
+  )
+}
+
+export default uiuxpage
