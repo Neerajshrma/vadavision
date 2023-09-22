23b41923fd14d54e2e1bd7339f9331252b08c471
unknown <prashant@vadavision.ca>
2023-09-22 10:59:31 +0530

    vadavision

---
+++ src/app/components/AboutUsPage/ServiceCards/MySlider.jsx
@@ -0,0 +1,53 @@
+// "use client";
+// import React from "react";
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import Cards from './Cards'
+// import SampleNextArrow from "./SampleNextArrow";
+// import SamplePrevArrow from "./SamplePrevArrow";
+
+// const data=[
+//     {
+//         img:'images/iconbranding.svg',
+//         servise1:'UI/UX and',
+//         servise2:'Brand Identity',
+//         read:'READ MORE'
+//     },
+//     {
+//         img:'images/iconbranding.svg',
+//         servise1:'UI/UX and',
+//         servise2:'Brand Identity',
+//         read:'READ MORE'
+//     }, {
+//         img:'images/iconbranding.svg',
+//         servise1:'UI/UX and',
+//         servise2:'Brand Identity',
+//         read:'READ MORE'
+//     }, {
+//         img:'images/iconbranding.svg',
+//         servise1:'UI/UX and',
+//         servise2:'Brand Identity',
+//         read:'READ MORE'
+//     }
+// ]
+
+// const MySlider = () => {
+
+   
+//   return (
+//     <>
+//         <div className=" realtive flex flex-row  px-40 responsive-width mobile-width ">
+
+
+//         {/* {data.map((element,index)=><Cards key={index} img={element.img} />)} */}
+
+//         <Slider images={data}/>
+
+
+//     </div>
+//     </>
+//   )
+// }
+
+// export default MySlider
