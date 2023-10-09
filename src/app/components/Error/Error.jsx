7bd138e139d4334a9b5bdabb10b1056bb8dc5d61
Neeraj sharma <shrmanerj.sn@gmail.com>
2023-10-09 15:17:33 +0530

    404 page added

---
+++ src/app/components/Error/Error.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className="h-[100vh]">
+      <img className="max-h-[100%] w-full" src="images/404.jpg" alt="" />
+    </div>
+  );
+};
+
+export default Error;
