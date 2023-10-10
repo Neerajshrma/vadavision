df9417a26960ab7c18a072176252e946bd3794c4
Neeraj sharma <shrmanerj.sn@gmail.com>
2023-10-10 18:50:32 +0530

    use client added in dropdown menu

--- src/app/components/CaseStudyPage/ProblemStatementPage/ProblemStament.jsx
+++ src/app/components/CaseStudyPage/ProblemStatementPage/ProblemStament.jsx
@@ -12,14 +12,13 @@ const ProblemStament = () => {
         </div>
         <div className="text-center flex justify-center  item-center font-semibold">
           <p className=" ">
-            "Users are increasingly looking for immersive and interactive ways
-            to capture and share their experiences through photos and videos.
-            While panoramic photos and videos offer a wider perspective, the
-            ability to capture a full 360-degree view of their surroundings
-            remains a challenge for many. Existing 360-degree camera apps often
-            lack user-friendly interfaces, precise control, and efficient
-            stitching algorithms, making the process cumbersome and
-            time-consuming.
+            Users are increasingly looking for immersive and interactive ways to
+            capture and share their experiences through photos and videos. While
+            panoramic photos and videos offer a wider perspective, the ability
+            to capture a full 360-degree view of their surroundings remains a
+            challenge for many. Existing 360-degree camera apps often lack
+            user-friendly interfaces, precise control, and efficient stitching
+            algorithms, making the process cumbersome and time-consuming.
           </p>
         </div>
       </div>
--- src/app/components/common/Footer/SocialMediaDropdown.jsx
+++ src/app/components/common/Footer/SocialMediaDropdown.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from "react";
 import { GoChevronDown, GoChevronUp } from "react-icons/go";
 import { AiFillInstagram } from "react-icons/ai";
