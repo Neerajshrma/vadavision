185c2cac3e3e39b3e0ef946c9e82d9aa858152a2
unknown <prashant@vadavision.ca>
2023-10-12 18:55:43 +0530

    send message form  responsive

--- src/app/components/ContactUsPage/SendMessagePage/SendMessage.jsx
+++ src/app/components/ContactUsPage/SendMessagePage/SendMessage.jsx
@@ -21,18 +21,16 @@ const SendMessage = () => {
         >
           <h1
             style={{ fontFamily: "Poppins" }}
-            className="message-responsiness hero-heading leading-[120%] font-semibold text-white whitespace-nowrap"
+            className="message-responsiness  leading-[120%] font-semibold text-white whitespace-nowrap"
           >
             Send a Message
           </h1>
           <div className="mt-5 pb-10  ">
-            <p
-              style={{ fontFamily: "Mulish" }}
-              className="mt-4 text-[#A9AFC3] display-message w-full font-light text-sm  "
-            >
-              We always try to implement our creative ideas at the highest
-              level. Tell us about your project and we will make it work.
-            </p>
+          <div className="w-6/12 mt-4 msg-info">
+          <p className="Mulish text-[#A9AFC3] text-base leading-[140%] msg-info">
+          We always try to implement our creative ideas at the highest level. Tell us about your project and we will make it work.
+          </p>
+        </div>
           </div>
         </div>
 
@@ -41,7 +39,7 @@ const SendMessage = () => {
             <input
               style={{ backgroundColor: "inherit" }}
               type="text"
-              className="text-sm border-none borderless-input text-white px-0"
+              className="text-sm border-none borderless-input text-white px-0 responsive-input"
               placeholder="Name"
             />
           </div>
@@ -49,7 +47,7 @@ const SendMessage = () => {
             <input
               style={{ backgroundColor: "inherit" }}
               type="email"
-              className="text-sm border-none borderless-input text-white px-0"
+              className="  text-sm border-none borderless-input responsive-input text-white px-0"
               placeholder="Email"
             />
           </div>
@@ -59,7 +57,7 @@ const SendMessage = () => {
             style={{ backgroundColor: "inherit" }}
             placeholder="Message"
             id=""
-            className="text-sm border-none borderless-input text-white px-0"
+            className="text-sm border-none responsive-input borderless-input text-white px-0"
             cols="54"
             rows="4"
           ></textarea>
