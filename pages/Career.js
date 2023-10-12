bb6a86dbef2ec6e220408c8f7613172b6ba1375f
Neeraj sharma <shrmanerj.sn@gmail.com>
2023-10-12 18:14:00 +0530

    clear contact us page conflit

---
+++ pages/Career.js
@@ -0,0 +1,12 @@
+import CarrierPage from '@/app/components/CarrierPage/CarrierPage'
+import React from 'react'
+
+const Career = () => {
+  return (
+    <div>
+        <CarrierPage />
+    </div>
+  )
+}
+
+export default Career
