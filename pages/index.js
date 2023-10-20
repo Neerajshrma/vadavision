9fec2201684f83f4f6b1142f7d286732709de6ee
Pankil Joshi <pankil@airbadge.us>
2023-10-20 15:03:56 +0530

    Added more keywords

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const index = () => {
         <Head>
         <title>Vadavision | Transforming Ideas into Reality | AI, Web Automation, Mobile Apps</title>
         <meta name="description" content="At Vadavision, we're a leading software company and startup studio based in India, specializing in staff/resource augmentation, AI, web automation applications, mobile apps, and cutting-edge UI/UX design. Our dedicated team of skilled developers ensures swift project delivery and cost-efficiency. Let us turn your ideas into reality." />
-        <meta name="keywords" content="Vadavision, Software Company, Startup Studio, Staff Augmentation, AI Development, Web Automation, Mobile App Development, UI/UX Design, Skilled Developers, Fast Turnaround, Cost-Efficiency, India-Based Company, Software Solutions, Innovation, Efficiency, Flutter, React Native, Hybrid Mobile Apps" />
+        <meta name="keywords" content="Vadavision, Software Company, Startup Studio, Staff Augmentation, AI Development, Web Automation, Mobile App Development, UI/UX Design, Skilled Developers, Fast Turnaround, Cost-Efficiency, India-Based Company, Software Solutions, Innovation, Efficiency, Flutter, React Native, Hybrid Mobile Apps, OpenAI API, Chatgpt, Sales AI bot" />
         </Head>
         <Homepage/>
     </div>
