80eea36ada6e6469af2e7de4d10235459ad9a200
unknown <prashant@vadavision.ca>
2023-10-20 15:06:02 +0530

    Footer Ui change and link updated

---
+++ src/app/components/common/Number.js
@@ -0,0 +1,13 @@
+const number= {
+
+    SatisfiedClients:'1000',
+    AwardWinning:'20',
+    TeamMembers:'12',
+    successfulProjects:'400',
+    ProjectComplete:'1000',
+    OurTeam:'233',
+    Ourclient:'1100'
+
+};
+
+export default number;
