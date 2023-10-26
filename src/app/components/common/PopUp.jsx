804040ff0afd2369c87505717e6310995501c507
Prashant Ranjan <persie@Prashants-MacBook-Air.local>
2023-10-26 19:11:44 +0530

    POP up fix

--- src/app/components/common/PopUp.jsx
+++ src/app/components/common/PopUp.jsx
@@ -3,22 +3,21 @@ import React, { useState } from "react";
 import SkeletonLoader from "./SkeletonLoader";
 
 const PopUp = () => {
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   return (
     <>
-   <div>
-              <div
-                className="calendly-inline-widget no-scrollbar overflow"
-                data-url="https://calendly.com/pankil_joshi/discovery?hide_event_type_details=1&hide_gdpr_banner=1"
-                style={{ minWidth: "570px", height: "500px" }}
-              ></div>
-              <Script
-                src="https://assets.calendly.com/assets/external/widget.js"
-                async={true}
-              ></Script>
-            </div>
-     
+      <div>
+        <div
+          className="calendly-inline-widget no-scrollbar overflow"
+          data-url="https://calendly.com/pankil_joshi/discovery?hide_event_type_details=1&hide_gdpr_banner=1"
+          style={{ minWidth: "570px", height: "500px" }}
+        ></div>
+        <Script
+          src="https://assets.calendly.com/assets/external/widget.js"
+          async={true}
+        ></Script>
+      </div>
     </>
   );
 };
