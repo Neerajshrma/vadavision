466a5b5e95bee9d59cf23d3389dbf30be3fcc26b
Kaushal Mehta <kaushalkumar304@gmail.com>
2023-10-27 17:52:02 +0530

    SSR and contact calendar

--- src/app/components/common/PopUp.jsx
+++ src/app/components/common/PopUp.jsx
@@ -2,19 +2,19 @@ import Script from "next/script";
 import React, { useState } from "react";
 
 const PopUp = () => {
-
   return (
     <>
       <div>
-        <div
+        <iframe
           className="calendly-inline-widget no-scrollbar overflow"
-          data-url="https://calendly.com/pankil_joshi/discovery?hide_event_type_details=1&hide_gdpr_banner=1"
+          src="https://calendly.com/pankil_joshi/discovery?hide_event_type_details=1&hide_gdpr_banner=1"
           style={{ minWidth: "570px", height: "500px" }}
-        ></div>
-        <Script
-          src="https://assets.calendly.com/assets/external/widget.js"
-          async={true}
-        ></Script>
+        >
+          <Script
+            src="https://assets.calendly.com/assets/external/widget.js"
+            async={true}
+          ></Script>
+        </iframe>
       </div>
     </>
   );
