63242a50c90f8d2442d12a05578db8a594edda22
Pankil Joshi <pankil@airbadge.us>
2023-10-28 18:12:18 +0530

    Increased time to load calander

--- src/app/components/common/PopUp.jsx
+++ src/app/components/common/PopUp.jsx
@@ -1,10 +1,18 @@
 import Script from "next/script";
 import React, { useEffect, useState } from "react";
+import SkeletonLoader from "./SkeletonLoader";
 
 const PopUp = ({ minWidth }) => {
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+      setTimeout(() => {
+        setLoading(false);
+      }, 500);
+    }, []);
   return (
     <>
-    <div style={{minWidth:'100%', height:'700px'}} className="calendly-inline-widget" data-url="https://calendly.com/pankil_joshi/discovery?hide_event_type_details=1&hide_gdpr_banner=1">
+    {loading ? <SkeletonLoader /> : ''}
+    <div style={{display: (loading ? 'none' : 'block'), minWidth:'100%', height:'700px'}} className="calendly-inline-widget" data-url="https://calendly.com/pankil_joshi/discovery?hide_event_type_details=1&hide_gdpr_banner=1">
       <iframe src={`https://calendly.com/pankil_joshi/discovery?embed_domain=${process.env.NEXT_PUBLIC_HOST}&embed_type=Inline&hide_event_type_details=1&hide_gdpr_banner=1`} width="100%" height="100%" frameborder="0" title="Select a Date &amp; Time - Calendly"></iframe>
     </div>
     </>
