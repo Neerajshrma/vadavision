9fe791a013e1aa0fa1c7bceb664c45f7b1cfdbb5
Neeraj sharma <shrmanerj.sn@gmail.com>
2023-10-31 17:02:51 +0530

    fixed animation

--- src/app/components/Homepage/Subscribeus/SubscribeUs.jsx
+++ src/app/components/Homepage/Subscribeus/SubscribeUs.jsx
@@ -2,47 +2,60 @@ import React, { useEffect } from "react";
 import { TbSend } from "react-icons/tb";
 import RippleEffect from "../../common/RippleEffect";
 import Button from "../../common/Button";
-import AOS from 'aos';
-import 'aos/dist/aos.css'; 
+import AOS from "aos";
+import "aos/dist/aos.css";
 const SubscribeUs = () => {
   useEffect(() => {
-    if (typeof document !== 'undefined') {
+    if (typeof document !== "undefined") {
       AOS.init({
         offset: 300,
         duration: 1000,
         once: true,
-        easing: 'ease',
+        easing: "ease",
       });
     }
   }, []);
   return (
-    <div className="flex flex-wrap subscribe-us" >
+    <div className="flex flex-wrap subscribe-us">
       <div className="w-6/12 bg-[#191C26] full-width-responsive px-40 responsive-width py-20">
         <div className="flex gap-3 items-center">
           <div className="bg-[#A9AFC3] w-[50px] h-[1px]"></div>
-          <div data-aos="fade-up" className="Montserrat text-sm font-normal leading-[150%] text-[#A9AFC3]">
+          <div
+            data-aos="fade-up"
+            className="Montserrat text-sm font-normal leading-[150%] text-[#A9AFC3]"
+          >
             newsletter
           </div>
         </div>
         <div className="mt-4">
-          <h1 data-aos="fade-up" className="Montserrat capitallize text-white text-5xl font-bold leading-[110%] heading">
+          <h1
+            data-aos="fade-up"
+            className="Montserrat capitallize text-white text-5xl font-bold leading-[110%] heading"
+          >
             be always in touch{" "}
             <span className="inline-block w-3 h-3 ml-[-12px] bg-[#DD4242] rounded-full"></span>
           </h1>
         </div>
-        <div className="mt-5"data-aos='fade-right'>
-          <p data-aos="fade-up" className="Montserrat text-[#A9AFC3] text-base font-normal leading-[170%] description">
+        <div className="mt-5" data-aos="fade-up">
+          <p
+            data-aos="fade-up"
+            className="Montserrat text-[#A9AFC3] text-base font-normal leading-[170%] description"
+          >
             We are grateful to know you are interested in our content and
             services. Subscribe
           </p>
         </div>
-        <div className="flex items-center gap-8 mt-5"data-aos='fade-right'>
-          <input data-aos="fade-up"
+        <div className="flex items-center gap-8 mt-5" data-aos="fade-up">
+          <input
+            data-aos="fade-up"
             className="subs-input text-white border-b border-[#3A3F50] outline-none p-1"
             type="email"
             placeholder="Email"
           />
-          <div data-aos="fade-up" className="overflow-hidden cursor-pointer rounded-full bg-[#DD4242]">
+          <div
+            data-aos="fade-up"
+            className="overflow-hidden cursor-pointer rounded-full bg-[#DD4242]"
+          >
             <RippleEffect>
               <button className="p-3">
                 <span className="text-white">
@@ -54,7 +67,7 @@ const SubscribeUs = () => {
         </div>
       </div>
       <div
-        className="full-width-responsive w-6/12 bg-cover" 
+        className="full-width-responsive w-6/12 bg-cover"
         style={{
           backgroundImage: "url(images/team.jpg)",
           fontFamily: "Montserrat",
@@ -72,7 +85,10 @@ const SubscribeUs = () => {
                 <p className="text-white text-sm font-semibold">
                   Sahil Salaria
                 </p>
-                <p data-aos="fade-up" className="mt-1 text-xs font-normal text-[#A9AFC3]">
+                <p
+                  data-aos="fade-up"
+                  className="mt-1 text-xs font-normal text-[#A9AFC3]"
+                >
                   CEO at Vadavision
                 </p>
               </div>
@@ -82,7 +98,8 @@ const SubscribeUs = () => {
                 backgroundImage: "url(images/icon-quote.png)",
                 backgroundPosition: "right bottom",
               }}
-              data-aos="fade-up"  className="w-[300px] pt-10 pb-20 text-base text-white font-normal bg-no-repeat bg-auto"
+              data-aos="fade-up"
+              className="w-[300px] pt-10 pb-20 text-base text-white font-normal bg-no-repeat bg-auto"
             >
               We never give up. Сhallenges make us stronger and more
               professional. Keep in mind. Beware
